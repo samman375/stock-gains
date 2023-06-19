efa1e377f40494e95e8888c6d6446a8224d21469
samman375 <samuel.thorley2000@gmail.com>
2023-06-19 21:50:35 +1000

    Initial commit

---
+++ gains.py
@@ -0,0 +1,56 @@
+from investments import Investments
+
+
+inv = Investments()
+
+print("stock-gains: Command-line stock valuation tool\n")
+print("Enter 'help' for list of commands.")
+
+while True:
+    command = input('Command: ').lower().strip()
+    if command == 'help' or command == 'h':
+        help = """
+        'b' or 'buy' :          Add new investment purchase
+        's' or 'sell':          Add new sale purchase
+        'v' or 'value':         Get total portfolio value
+        't' or 'ticker-value':  Get value of specific owned ticker
+        'hs' or 'history':      Investment history
+        'd' or 'dividend':      Add received dividend
+        'e' or 'estimate':      Get dividend estimate for next 12 months
+        'h' or 'help':          Help
+        'q' or 'quit':          Quit
+        """
+        print(help)
+
+    elif command == 'buy' or command == 'b':
+        ticker = input('Ticker: ').strip()
+        price = float(input('Price: ').strip())
+        volume = int(input('Volume: ').strip())
+        brokerage = float(input('Brokerage: ').strip())
+        date = input('Date (DD-MM-YYYY): ').strip()
+        inv.buyInvestment(ticker, price, volume, brokerage, date)
+
+    elif command == 'sell' or command == 's':
+        continue
+
+    elif command == 'ticker-value' or command == 't':
+        ticker = input("Ticker Code: ").strip()
+        inv.investmentValue(ticker)
+
+    elif command == 'value' or command == 'v':
+        inv.listInvestments()
+    
+    elif command == 'history' or command == 'h':
+        continue
+
+    elif command == 'dividend' or command == 'd':
+        continue
+
+    elif command == 'estimate' or command == 'e':
+        continue
+
+    elif command == 'quit' or command == 'q':
+        break
+
+    else:
+        print("Invalid command. Enter 'help' for list of commands.")
---
+++ investments.py
@@ -0,0 +1,275 @@
+import os
+import json
+import requests
+import pandas as pd
+import yfinance as yf
+
+INVESTMENT_FILE_NAME = 'store/investments.json'
+HISTORY_FILE_NAME = 'store/investment_history.json'
+DIVIDEND_FILE = '/store/dividend_history.json'
+
+# History File: tradeId:int, ticker:str, price:float, volume:int, brokerage:float, date:str
+# Investment file: ticker, volume, cost, totalBrokerage, dividend
+# Dividend file: ticker, date, value
+
+
+class Investments:
+    def __init__(self):
+        self.investments = self.getInvestments()
+        self.history = self.getHistory()
+        self.maxId = self.getMaxId()
+        self.dividends = self.getDividends()
+    
+    ###########
+    # HELPERS #
+    ###########
+
+    def getHistory(self):
+        """
+        Get investment history from file
+        """
+        if os.path.isfile(HISTORY_FILE_NAME):
+            with open(HISTORY_FILE_NAME, 'r') as f:
+                return json.load(f)
+        else:
+            return {}
+    
+    def getInvestments(self):
+        """
+        Get investments from file
+        """
+        if os.path.isfile(INVESTMENT_FILE_NAME):
+            with open(INVESTMENT_FILE_NAME, 'r') as f:
+                return json.load(f)
+        else:
+            return {}
+    
+    def getDividends(self):
+        """
+        Get dividends from file
+        """
+        if os.path.isfile(DIVIDEND_FILE):
+            with open(DIVIDEND_FILE, 'r') as f:
+                return json.load(f)
+        else:
+            return {}
+
+    def getMaxId(self):
+        """
+        Gets largest trade ID
+        """
+        if len(self.history.keys()) > 0:
+            return max([int(tradeId) for tradeId in self.history])
+        else:
+            return 0
+    
+    def getTickerData(self, tickers:str):
+        """
+        Get data for tickers from Yahoo Finance API
+        """
+        # TODO: Fix dividends
+
+        tickerData = yf.Tickers(tickers)
+        data = {}
+        for ticker in tickers.split():
+            # print(tickerData.tickers[ticker].info)
+            data[ticker] = {}
+            data[ticker]['price'] = tickerData.tickers[ticker].info['ask']
+            data[ticker]['fullName'] = tickerData.tickers[ticker].info['longName']
+            # data[ticker]['annualDividends'] = tickerData.tickers[ticker].info['trailingAnnualDividendYield']
+            data[ticker]['quoteType'] = tickerData.tickers[ticker].info['quoteType']
+
+        return data
+
+    def updateInvestmentFile(self):
+        with open(INVESTMENT_FILE_NAME, 'w') as f:
+            json.dump(self.investments, f)
+            f.close()
+
+    def updateHistoryFile(self):
+        with open(HISTORY_FILE_NAME, 'w') as f:
+            json.dump(self.history, f)
+            f.close()
+    
+    def updateDividendFile(self):
+        with open(DIVIDEND_FILE, 'w') as f:
+            json.dump(self.dividends, f)
+            f.close()
+
+    def outputTickerHeader(self):
+        print(f"\nTicker    {'Full Name'.ljust(60, ' ')}  Price    Cost      Value     %Gain    %NetGain  Gain      Net Gain  Dividends")
+
+
+    def outputTickerValue(self, data:object, ticker:str):
+        price = data[ticker]['price']
+        fullName = data[ticker]['fullName']
+        volume = self.investments[ticker]['volume']
+        cost = self.investments[ticker]['cost']
+        totalBrokerage = self.investments[ticker]['totalBrokerage']
+        dividend = self.investments[ticker]['dividend']
+        value = price * volume
+        percGain = (value / (cost - totalBrokerage) - 1) * 100
+        netPercGain = (value / cost - 1) * 100
+        gain = value - (cost - totalBrokerage)
+        netGain = value - cost
+
+        print(f"{ticker.ljust(8, ' ')}  {fullName.ljust(60, ' ')}  {str(round(price, 2)).ljust(7, ' ')}  {str(round(cost, 2)).ljust(8, ' ')}  {str(round(value, 2)).ljust(8, ' ')}  {(str(round(percGain, 2)) + '%').ljust(7, ' ')}  {(str(round(netPercGain, 2)) + '%').ljust(7, ' ')}   {str(round(gain, 2)).ljust(8, ' ')}  {str(round(netGain, 2)).ljust(8, ' ')}  {str(round(dividend, 2)).ljust(7, ' ')}")
+
+        return {'cost': cost, 'value': value, 'dividend': dividend, 'gain': gain, 'net_gain': netGain, 'brokerage': totalBrokerage}
+
+    def sortByCost(x, y):
+        if x['cost'] >= y['cost']:
+            return x
+        else:
+            return y
+
+    ###########
+    # METHODS #
+    ###########
+
+    def buyInvestment(self, ticker:str, price:float, volume:int, brokerage:float, date:str):
+        """
+        Add investment to file and update investments
+        """
+        tradeId = int(self.maxId) + 1
+        self.maxId = tradeId
+
+        trade = {}
+        trade['ticker'] = ticker
+        trade['price'] = price
+        trade['volume'] = volume
+        trade['brokerage'] = brokerage
+        trade['date'] = date
+        trade['status'] = 'buy'
+
+        self.history[tradeId] = trade
+        self.updateHistoryFile()
+
+        cost = volume * price + brokerage
+        if ticker in self.investments.keys():
+            self.investments[ticker]['volume'] += volume
+            self.investments[ticker]['cost'] += cost
+            self.investments[ticker]['totalBrokerage'] += brokerage
+        else:
+            self.investments[ticker] = {}
+            self.investments[ticker]['volume'] = volume
+            self.investments[ticker]['cost'] = cost
+            self.investments[ticker]['totalBrokerage'] = brokerage
+            self.investments[ticker]['dividend'] = 0
+        
+        self.updateInvestmentFile()
+
+        print(f"Trade ID: {tradeId}. Purchased {volume} of {ticker} at ${price} per share on {date} with a ${brokerage} brokerage fee\n")
+    
+
+    def sellInvestment(self, tradeId:int, ticker:str, price:float, volume:int, brokerage:float, date:str):
+        """
+        Sell investment from file and update investments
+        """
+        allSold = False
+
+        profit = volume * price - brokerage
+        if ticker in self.investments.keys() and volume > self.investments[ticker]['volume']:
+            self.investments[ticker]['volume'] -= volume
+            self.investments[ticker]['cost'] -= profit
+            self.investments[ticker]['totalBrokerage'] += brokerage
+        elif ticker in self.investments.keys() and volume == self.investments[ticker]['volume']:
+            self.investments.remove(ticker)
+            allSold = True
+        else:
+            print("Error: invalid ticker or insufficient volume")
+
+        self.updateInvestmentFile()
+
+        trade = {}
+        trade['ticker'] = ticker
+        trade['price'] = price
+        trade['volume'] = volume
+        trade['brokerage'] = brokerage
+        trade['date'] = date
+        trade['status'] = 'sell'
+        
+        self.history[tradeId] = trade
+        self.updateHistoryFile()
+
+
+        print(f"Trade ID: {tradeId}. Sold {volume} of {ticker} at ${price} per share on {date} with a ${brokerage} brokerage fee\n")
+        if allSold:
+            print(f"-- All existing {volume} stock sold.\n")
+
+
+    def listInvestments(self):
+        """
+        Output investments to command line sorted by value + do with and without dividends
+        """
+        # TODO: sort by value
+
+        tickers = ""
+        for ticker in self.investments.keys():
+            tickers += f"{ticker} "
+    
+        data = self.getTickerData(tickers.strip())
+
+        self.outputTickerHeader()
+
+        totalCost = 0
+        totalValue = 0
+        totalDividend = 0
+        totalGain = 0
+        totalNetGain = 0
+        totalBrokerage = 0
+
+        tickers = [k for k, _ in sorted(self.investments.items(), key=lambda x:x[1]['cost'], reverse=True)]
+        for ticker in tickers:
+        # for ticker in self.investments.keys():
+            values = self.outputTickerValue(data, ticker)
+            totalCost += values['cost']
+            totalValue += values['value']
+            totalDividend += values['dividend']
+            totalGain += values['gain']
+            totalNetGain += values['net_gain']
+            totalBrokerage += values['brokerage']
+        
+        percGain = (totalValue / (totalCost - totalBrokerage) - 1) * 100
+        netPercGain = (totalValue / totalCost - 1) * 100
+        print(f"          {'Total'.ljust(69, ' ')}  {str(round(totalCost, 2)).ljust(8, ' ')}  {str(round(totalValue, 2)).ljust(8, ' ')}  {(str(round(percGain, 2)) + '%').ljust(7, ' ')}  {(str(round(netPercGain, 2)) + '%').ljust(7, ' ')}   {str(round(totalGain, 2)).ljust(8, ' ')}  {str(round(totalNetGain, 2)).ljust(8, ' ')}  {str(round(totalDividend, 2)).ljust(7, ' ')}")
+        print()
+
+
+    def investmentValue(self, ticker:str):
+        """
+        Get value output for specific ticker + do with and without dividends
+        """
+        data = self.getTickerData(ticker)
+        self.outputTickerHeader()
+        self.outputTickerValue(data, ticker)
+        print()
+        
+        # TODO: add invalid ticker error handling
+
+
+    def investmentHistory(self):
+        """
+        Output investments to command line sorted by date
+        """
+        # TODO:
+        
+        return
+
+
+    def addDividend(self, date, ticker, value):
+        """
+        Add dividend received
+        """
+        # TODO:
+        
+        return
+    
+
+    def estimateDividends(self, months: int):
+        """
+        Estimated dividends in next 3 months, 6 months, year
+        """
+        # TODO:
+        
+        return
